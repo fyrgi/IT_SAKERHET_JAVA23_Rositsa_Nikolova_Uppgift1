c7ea4ea94b1aada694501d73cbd6bd4c10e08a8f
Rositsa Nikolova <rositsa.se@gmail.com>
2024-10-08 22:18:23 +0200

    Update delivery.sql
    Database got updated and simplified.

--- delivery.sql
+++ delivery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 09:26 PM
+-- Generation Time: Oct 08, 2024 at 10:16 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -33,26 +33,20 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(60) NOT NULL,
+  `password` varchar(128) NOT NULL,
   `name` varchar(255) NOT NULL,
   `phone` varchar(16) NOT NULL,
-  `agree_to_terms` tinyint(4) NOT NULL,
-  `address_line_one` varchar(300) NOT NULL,
-  `adress_line_two` varchar(300) DEFAULT NULL,
-  `post_code` varchar(12) NOT NULL,
-  `comment` text DEFAULT NULL,
-  `city` varchar(30) NOT NULL,
-  `country` varchar(30) NOT NULL,
-  `address_line_two` varchar(255) DEFAULT NULL,
-  `agreement` bit(1) NOT NULL
+  `agreement` varchar(6) DEFAULT NULL,
+  `address` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `password`, `name`, `phone`, `agree_to_terms`, `address_line_one`, `adress_line_two`, `post_code`, `comment`, `city`, `country`, `address_line_two`, `agreement`) VALUES
-(1, 'test@test.bg', '123123', 'Test Testsson', '3298745', 1, 'Address 1', NULL, '38247', 'ashdfu3y2uhfkajshd ', 'Malmö', 'Sweden', NULL, b'0');
+INSERT INTO `user` (`id`, `email`, `password`, `name`, `phone`, `agreement`, `address`) VALUES
+(1, '7', '91c7e2d523f4bced3202248e63e36ac0dad807a97f73043a53a7eb5b6eacde7d7902699be42c8a8e46fbbb4501726517e86b22c56a189f7625a6da49081b2451', '7', '7', 'true', '7'),
+(2, '1', '91c7e2d523f4bced3202248e63e36ac0dad807a97f73043a53a7eb5b6eacde7d6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', '1', '1', 'true', '1');
 
 --
 -- Indexes for dumped tables
@@ -73,7 +67,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
